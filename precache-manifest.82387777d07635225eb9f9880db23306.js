ee006d36bac704803f31f4c325a18aad60bf5047
IlyasIt <ilyas.musin.172001@mail.ru>
2020-10-05 06:00:31 +0300

    Updates

---
+++ precache-manifest.82387777d07635225eb9f9880db23306.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799dd8642995f1b4040a7c752c0d9bbe",
+    "url": "/towner/index.html"
+  },
+  {
+    "revision": "1a9518e99b8d4c6803a6",
+    "url": "/towner/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "2defdb90c53036a3a832",
+    "url": "/towner/static/css/main.d6fcfd69.chunk.css"
+  },
+  {
+    "revision": "1a9518e99b8d4c6803a6",
+    "url": "/towner/static/js/2.c2d2a6c3.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/towner/static/js/2.c2d2a6c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2defdb90c53036a3a832",
+    "url": "/towner/static/js/main.5e402d96.chunk.js"
+  },
+  {
+    "revision": "e9de405d4a49c8aabfee",
+    "url": "/towner/static/js/runtime-main.01d830aa.js"
+  },
+  {
+    "revision": "4818ec8bde167a55f24833f52cc1046e",
+    "url": "/towner/static/media/AvatarApp.4818ec8b.png"
+  },
+  {
+    "revision": "7aac83235ef32a95ccc7dc321d150aee",
+    "url": "/towner/static/media/arrow.7aac8323.svg"
+  }
+]);
